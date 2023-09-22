4185e1a995896e5ee65e053aa66f5e065b3433dd
Zawahir <139978657+MuhammadZawahir@users.noreply.github.com>
2023-09-22 16:08:22 +0500

    Exercise 4

---
+++ index.ts
@@ -0,0 +1,9 @@
+/*Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”*/
+
+
+
+
+let famousQuote:string="Work, work and work and we are bound to succeed"
+console.log(`Quaid e Azam said:${famousQuote}`)
